10285953078eae9ee1111cb0b8ffe58a4fbe42f1
GersonEC <gersonenriquez94@gmail.com>
2022-03-04 17:29:21 +0100

    handling auth state

--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -1,28 +1,65 @@
 import React, { createContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const AuthContext = createContext();
 const { Provider } = AuthContext;
 
 const AuthProvider = ({ children }) => {
+  const history = useHistory();
+  const token = localStorage.getItem('token');
+  const userInfo = localStorage.getItem('userInfo');
+  const expiresAt = localStorage.getItem('expiresAt');
+
   const [authState, setAuthState] = useState({
-    token: null,
-    expiresAt: null,
-    userInfo: {}
+    token,
+    expiresAt,
+    userInfo: userInfo ? JSON.parse(userInfo) : {}
   })
 
   const setAuthInfo = ({ token, userInfo, expiresAt }) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('userInfo', JSON.stringify(userInfo));
+    localStorage.setItem('expiresAt', expiresAt);
     setAuthState({
       token,
       userInfo,
       expiresAt
     })
+    history.push("/login");
   }
+
+  const logout = () => {
+    localStorage.removeItem('userInfo')
+    localStorage.removeItem('token')
+    localStorage.removeItem('expiresAt')
+    setAuthState({
+      token: null,
+      expiresAt: null,
+      userInfo: {}
+    })
+    history.push('/login');
+  }
+
+  const isAuthenticated = () => {
+    if (!authState.token || !authState.expiresAt) {
+      return false;
+    }
+    return new Date().getTime() / 1000 < authState.expiresAt
+  }
+
+  const isAdmin = () => {
+    return authState.userInfo.role === 'admin';
+  }
+
 
   return (
     <Provider
       value={{
         authState,
-        setAuthState: authInfo => setAuthInfo(authInfo)
+        setAuthState: authInfo => setAuthInfo(authInfo),
+        isAuthenticated,
+        logout,
+        isAdmin
       }}
     >
       {children}
